fc465d58aa21363d11c32b7756f3c2b54cab1914
yamauchitomoya <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2021-01-12 10:02:01 +0900

    20210112
    update

--- news/wordpress/wp-content/themes/lovecraft/full-width-page-template.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/* Template Name: Full Width Page Template */
-
-// Include the regular singular.php template, and use a conditional to hide the sidebar
-get_template_part( 'singular' ); ?>
